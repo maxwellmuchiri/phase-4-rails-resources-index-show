087d0a9112a888e9bbc58f7121690a661663b3cb
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-03-23 17:27:27 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    get '/birds', to: 'birds#index'
+    get '/birds/:id', to: 'birds#show'
+  end
+
+  Rails.application.routes.draw do
+    resources :birds
+  end
+  Rails.application.routes.draw do
+    resources :birds, only: [:index, :show]
+  end
+
 end
